250d27d9a2e3a113fbed69826cabf86f9081addf
hunterlegend <850899364@qq.com>
2018-03-27 17:52:29 +0800

    定单交易号小时大写

--- FycnApi/Controllers/PaymentController.cs
+++ FycnApi/Controllers/PaymentController.cs
@@ -203,7 +203,7 @@ namespace FycnApi.Controllers
         {
             Random ran = new Random();
             int RandKey = ran.Next(1000, 9999);
-            string out_trade_no = DateTime.Now.ToString("yyyyMMddhhmmssffff") + RandKey.ToString();
+            string out_trade_no = DateTime.Now.ToString("yyyyMMddHHmmssffff") + RandKey.ToString();
             return out_trade_no;
         }
 
