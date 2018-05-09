78e6481ed7100d60e8b37a618e027ff37e88b6a0
hunterlegend <850899364@qq.com>
2018-05-09 16:53:56 +0800

    wechat pay

---
+++ Fycn.Model/Wechat/ProductPayModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fycn.Model.Wechat
+{
+    public class ProductPayModel
+    {
+        public string TradeNo
+        {
+            get;
+            set;
+        }
+
+        public string TradeAmount
+        {
+            get;
+            set;
+        }
+
+        public string WaresId
+        {
+            get;
+            set;
+        }
+
+        public int Number
+        {
+            get;
+            set;
+        }
+
+        public string WaresName
+        {
+            get;
+            set;
+        }
+    }
+}
--- FycnApi/Controllers/WechatController.cs
+++ FycnApi/Controllers/WechatController.cs
@@ -15,6 +15,9 @@ using Fycn.Service;
 using log4net;
 using Fycn.Model.Wechat;
 using Fycn.Model.Product;
+using System.Xml;
+using System.Text;
+using Fycn.PaymentLib;
 
 namespace FycnApi.Controllers
 {
@@ -103,6 +106,145 @@ namespace FycnApi.Controllers
         {
             IWechat iwechat = new WechatService();
             return Content(iwechat.GetProdcutByTypeAndClient(typeId,clientId));
+        }
+
+        //微信支付
+        public ResultObj<PayStateModel> PostDataW(string clientId, [FromBody]List<string> lstWaresId)
+        {
+            try
+            {
+                var log = LogManager.GetLogger("FycnApi", "wechat pay");
+               
+                IPay _ipay = new PayService();
+                //移动支付赋值
+                WxPayConfig payConfig = _ipay.GenerateConfigModelWByClientId(clientId);
+                payConfig.NOTIFY_URL= PathConfig.NotidyAddr + "/Wechat/PostPayResultW"; //结果通知方法
+                JsApi jsApi = new JsApi();
+                PayModel payInfo = new PayModel();
+                //JsApi.payInfo = new PayModel();
+                //生成code 根据code取微信支付的openid和access_token
+                jsApi.GetOpenidAndAccessToken("-1", payConfig, payInfo, "/wechat.html#/pay?clientId=" + clientId, "");
+
+                PayStateModel payState = new PayStateModel();
+
+
+                //JSAPI支付预处理
+
+                string result = HttpService.Get(payInfo.redirect_url);
+                log.Info("mmmmmmmmmmmmmmmm:" + result);
+                //生成交易号
+                payInfo.trade_no = PayHelper.GeneraterTradeNo();
+                //取商品信息
+
+
+                decimal totalFee = 0;
+                string productNames = string.Empty;
+                List<ProductModel> lstProduct = new List<ProductModel>();
+               
+
+
+                payInfo.product_name = productNames.Length > 25 ? productNames.Substring(0, 25) : productNames;
+
+                //string total_fee = "1";
+                //检测是否给当前页面传递了相关参数
+
+                // 1.先取购买商品的详情返回给用户   并缓存到页面   2.支付成功后跳转到支付结果页并把缓存数据插入到销售记录表
+                //若传递了相关参数，则调统一下单接口，获得后续相关接口的入口参数 
+
+                // jsApiPay.openid = openid;
+
+                payInfo.total_fee = Convert.ToInt32((totalFee * 100));
+                //payInfo.jsonProduct = JsonHandler.GetJsonStrFromObject(keyJsonInfo, false);
+
+                //写入交易中转
+                /*
+                RedisHelper helper = new RedisHelper(0);
+                
+                helper.StringSet(JsApi.payInfo.trade_no.Trim(), JsApi.payInfo.jsonProduct, new TimeSpan(0, 10, 30));
+                */
+                // FileHandler.WriteFile("data/", JsApi.payInfo.trade_no + ".wa", JsApi.payInfo.jsonProduct);
+
+                WxPayData unifiedOrderResult = jsApi.GetUnifiedOrderResult(payInfo, payConfig);
+                // Log.Write("GetDataW", "step step");
+                string wxJsApiParam = jsApi.GetJsApiParameters(payConfig, payInfo);//获取H5调起JS API参数       
+                payState.RequestState = "1";
+                payState.ProductJson = JsonHandler.GetJsonStrFromObject(lstProduct, false);
+                payState.RequestData = wxJsApiParam;
+
+
+
+                return Content(payState);
+
+            }
+            catch (Exception ex)
+            {
+                PayStateModel payStateError = new PayStateModel();
+                payStateError.RequestState = "3";
+                payStateError.RequestData = ex.Message;
+                return Content(payStateError);
+            }
+            return Content(new PayStateModel());
+        }
+
+        // 微信支付结果
+        public string PostPayResultW()
+        {
+            try
+            {
+                var request = Fycn.Utility.HttpContext.Current.Request;
+                int len = (int)request.ContentLength;
+                byte[] b = new byte[len];
+                Fycn.Utility.HttpContext.Current.Request.Body.Read(b, 0, len);
+                string postStr = Encoding.UTF8.GetString(b);
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(postStr);
+                // 商户交易号
+                XmlNode tradeNoNode = xmlDoc.SelectSingleNode("xml/out_trade_no");
+                /*
+                RedisHelper helper = new RedisHelper(0);
+                
+                if (!helper.KeyExists(tradeNoNode.InnerText))
+                {
+                    return Content(1);
+                }
+                */
+                /*
+                IMachine _imachine = new MachineService();
+                if (_imachine.GetCountByTradeNo(tradeNoNode.InnerText) > 0)
+                {
+                    return Content(1);
+                }
+                */
+
+
+                //支付结果
+                XmlNode payResultNode = xmlDoc.SelectSingleNode("xml/result_code");
+                if (payResultNode.InnerText.ToUpper() == "SUCCESS")
+                {
+                    /*******************************放到微信支付通知参数里，因参数只支付最大128个字符长度，所以注释修改*****************************/
+                    //XmlNode tunnelNode = xmlDoc.SelectSingleNode("xml/attach");
+                    //KeyJsonModel keyJsonModel = JsonHandler.GetObjectFromJson<KeyJsonModel>(tunnelNode.InnerText);
+                    XmlNode attachNode = xmlDoc.SelectSingleNode("xml/attach");
+                    string jsonProduct = attachNode.InnerText;//helper.StringGet(tradeNoNode.InnerText);
+                    XmlNode mchIdNode = xmlDoc.SelectSingleNode("xml/mch_id"); // 商户号
+                    XmlNode openidNode = xmlDoc.SelectSingleNode("xml/openid"); //买家唯一标识
+                    XmlNode isSubNode = xmlDoc.SelectSingleNode("xml/is_subscribe"); // 是否为公众号关注者
+                    XmlNode timeEndNode = xmlDoc.SelectSingleNode("xml/time_end"); // 是否为公众号关注者
+                                                                                   //string jsonProduct = FileHandler.ReadFile("data/" + tradeNoNode.InnerText + ".wa");
+
+                    KeyJsonModel keyJsonModel = JsonHandler.GetObjectFromJson<KeyJsonModel>(jsonProduct);
+                    IMachine _imachine = new MachineService();
+                    int result = _imachine.PostPayResultW(keyJsonModel, tradeNoNode.InnerText, mchIdNode.InnerText, openidNode.InnerText, isSubNode.InnerText, timeEndNode.InnerText);
+
+                }
+                return "<xml><return_code><![CDATA[SUCCESS]]></return_code></xml>";
+            }
+            catch (Exception ex)
+            {
+                return "<xml><return_code><![CDATA[FAIL]]></return_code></xml>";
+            }
+
+            //File.WriteAllText(@"c:\text.txt", postStr); 
         }
     }
 }
