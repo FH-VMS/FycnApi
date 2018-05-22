7044cc92fd6632ff0090bc509e61a977fcbd4127
hunterlegend <850899364@qq.com>
2018-05-22 14:59:01 +0800

    wechat setting

---
+++ Fycn.Model/Wechat/WebSettingModel.cs
@@ -0,0 +1,46 @@
+﻿using Fycn.Model.Sys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fycn.Model.Wechat
+{
+    [Table("table_wechat_info")]
+    public class WebSettingModel
+    {
+        [Column(Name = "id")]
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "client_id")]
+        public string ClientId
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "service_json")]
+        public string ServiceJson
+        {
+            get;
+            set;
+        }
+
+        [Column(Name = "carousel_json")]
+        public string CarouselJson
+        {
+            get;
+            set;
+        }
+    }
+}
