77e850f3518af84ab673636bd2c892632b8eec24
hunterlegend <850899364@qq.com>
2018-05-22 17:03:00 +0800

    web setting

---
+++ Fycn.Service/WebSettingService.cs
@@ -0,0 +1,11 @@
+﻿using Fycn.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fycn.Service
+{
+    public class WebSettingService : AbstractService, IWebSetting
+    {
+    }
+}
