1792a0e96d5bb377e372ee3e9f59428083977bba
hunterlegend <850899364@qq.com>
2018-07-17 16:22:32 +0800

    通知url改成域名

--- Fycn.PaymentLib/PathConfig.cs
+++ Fycn.PaymentLib/PathConfig.cs
@@ -10,9 +10,9 @@ namespace Fycn.PaymentLib
         //public static string DomainConfig = "http://www.fy-cn.top/p";
         public static string DomainConfig = "http://wechat.markhsiu.com/p";
 
-        public static string NotidyAddr = "http://120.27.217.224:8088/api";//测试
+        public static string NotidyAddr = "http://wechat.markhsiu.com:8088/api";//测试
 
-        //public static string NotidyAddr = "http://106.14.190.9:8088/api";//正式
+        //public static string NotidyAddr = "http://www.fy-cn.top:8088/api";//正式
         public static string RootAliMchId = "2088621838347114"; //分账的支付宝商户号
         public static string RootWeixinMchId = "1433899402"; //分账的微信商户号
     }
